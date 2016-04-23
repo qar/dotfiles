89cc59295adb106669366612fbf601177aade9c9
Qiao Anran <qiaoanran@gmail.com>
2016-04-23 10:30:28 +0800

    add install.sh script

---
+++ install.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+cd ~/.vim/bundle
+git clone git://github.com/altercation/vim-colors-solarized.git
+mv vim-colors-solarized ~/.vim/bundle/
