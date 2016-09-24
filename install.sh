f23a4c420f3ce3ac39c7d21150aa96d758468e7c
Qiao Anran <qiaoanran@gmail.com>
2016-09-24 10:54:01 +0800

    install tmux

--- install.sh
+++ install.sh
@@ -14,3 +14,12 @@ mv ~/.bash_profile ~/.bash_profile.backup
 ln -s ./bash_profile ~/.bash_profile
 
 npm install -g commitizen
+
+# Install Tmux
+# https://github.com/tmux/tmux
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure && make
+
+# Config tmux
