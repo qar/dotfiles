b7fa2f5601e2abf4a7366787050fb04c90d12a23
Qiao Anran <qiaoanran@gmail.com>
2018-07-23 19:04:59 +0800

    add files

---
+++ tmux/install.sh
@@ -0,0 +1,3 @@
+brew install tmux
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+ln -s $(realpath ./tmux.conf) ~/.tmux.conf
---
+++ vim/install.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -s $(realpath ./ftplugin) ~/.vim/ftplugin
+ln -s $(realpath ./vimrc) ~/.vimrc
